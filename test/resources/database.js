68b005d0447aa9c2766e72c9f95c9ca4a11c6c32
Alberto Valle <alberto.valle@qrvey.com>
2021-05-20 19:05:57 -0500

    ⭐️ new: Adding replacement sample

--- test/resources/database.js
+++ test/resources/database.js
@@ -10,6 +10,22 @@ const mysqlData = {
     }
 };
 
+const postgresData = {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    port: process.env.POSTGRES_PORT,
+    password: process.env.POSTGRES_PASSWORD,
+    databases: {
+        demo: {
+            tableName: process.env.POSTGRES_TABLE_NAME,
+            tableKey: process.env.POSTGRES_TABLE_KEY,
+            tableSchema: process.env.POSTGRES_TABLE_SCHEMA,
+            databaseName: process.env.POSTGRES_DATABASE_NAME,
+        }
+    }
+};
+
 module.exports = {
     mysqlData,
+    postgresData,
 };
